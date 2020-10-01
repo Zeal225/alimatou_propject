f3dcc7465446266ecf50d852d1fe9b7e43fe7370
Zeal225 <CoolibalyDeveloper2015@gmail.com>
2020-10-01 01:07:14 +0000

    changes: traitement des images

--- admin1/config.php
+++ admin1/config.php
@@ -7,7 +7,7 @@
 // database Connection variables
 define('HOST', 'localhost'); // Database host name ex. localhost
 define('USER', 'root'); // Database user. ex. root ( if your on local server)
-define('PASSWORD', 'root'); // user password  (if password is not set for user then keep it empty )
+define('PASSWORD', ''); // user password  (if password is not set for user then keep it empty )
 define('DATABASE', 'restaurant'); // Database Database name
 
 function DB()
